66ce1ce94b93bd46263640cf261d66fdf35a0b2c
iselcq <iselcq@gmail.com>
2022-06-13 20:57:27 -0500

    Optimizando el proyecto

--- Simulador Interactivo/script.js
+++ Simulador Interactivo/script.js
@@ -18,30 +18,30 @@ let horaSelect = document.getElementById("hora");
 
 
 
-const nuevasHorasJSON = `[{ "value": "00:00", "text": "00:00 AM" }, { "value": "00:30", "text": "00:30 AM" },
-{ "value": "01:00", "text": "01:00 AM" }, { "value": "01:30", "text": "01:30 AM" },
-{ "value": "02:00", "text": "02:00 AM" }, { "value": "02:30", "text": "02:30 AM" },
-{ "value": "03:00", "text": "03:00 AM" }, { "value": "03:30", "text": "03:30 AM" },
-{ "value": "04:00", "text": "04:00 AM" }, { "value": "04:30", "text": "04:30 AM" },
-{ "value": "05:00", "text": "05:00 AM" }, { "value": "05:30", "text": "05:30 AM" },
-{ "value": "06:00", "text": "06:00 AM" }, { "value": "06:30", "text": "06:30 AM" },
-{ "value": "07:00", "text": "07:00 AM" }, { "value": "07:30", "text": "07:30 AM" },
-{ "value": "08:00", "text": "08:00 AM" }, { "value": "08:30", "text": "08:30 AM" },
-{ "value": "09:00", "text": "09:00 AM" }, { "value": "09:30", "text": "09:30 AM" },
-{ "value": "10:00", "text": "10:00 AM" }, { "value": "10:30", "text": "10:30 AM" },
-{ "value": "11:00", "text": "11:00 AM" }, { "value": "11:30", "text": "11:30 AM" },
-{ "value": "12:00", "text": "12:00 AM" }, { "value": "12:30", "text": "12:30 AM" },
-{ "value": "13:00", "text": "13:00 AM" }, { "value": "13:30", "text": "13:30 AM" },
-{ "value": "14:00", "text": "14:00 AM" }, { "value": "14:30", "text": "14:30 AM" },
-{ "value": "15:00", "text": "15:00 AM" }, { "value": "15:30", "text": "15:30 AM" },
-{ "value": "16:00", "text": "16:00 AM" }, { "value": "16:30", "text": "16:30 AM" },
-{ "value": "17:00", "text": "17:00 AM" }, { "value": "17:30", "text": "17:30 AM" },
-{ "value": "18:00", "text": "18:00 AM" }, { "value": "18:30", "text": "18:30 AM" },
-{ "value": "19:00", "text": "19:00 AM" }, { "value": "19:30", "text": "19:30 AM" },
-{ "value": "20:00", "text": "20:00 AM" }, { "value": "20:30", "text": "20:30 AM" },
-{ "value": "21:00", "text": "21:00 AM" }, { "value": "21:30", "text": "21:30 AM" },
-{ "value": "22:00", "text": "22:00 AM" }, { "value": "22:30", "text": "22:30 AM" },
-{ "value": "23:00", "text": "23:00 AM" }, { "value": "23:30", "text": "23:30 AM" }]`
+const nuevasHorasJSON = `[{ "value": "00:00", "text": "00:00" }, { "value": "00:30", "text": "00:30" },
+{ "value": "01:00", "text": "01:00" }, { "value": "01:30", "text": "01:30" },
+{ "value": "02:00", "text": "02:00" }, { "value": "02:30", "text": "02:30" },
+{ "value": "03:00", "text": "03:00" }, { "value": "03:30", "text": "03:30" },
+{ "value": "04:00", "text": "04:00" }, { "value": "04:30", "text": "04:30" },
+{ "value": "05:00", "text": "05:00" }, { "value": "05:30", "text": "05:30" },
+{ "value": "06:00", "text": "06:00" }, { "value": "06:30", "text": "06:30" },
+{ "value": "07:00", "text": "07:00" }, { "value": "07:30", "text": "07:30" },
+{ "value": "08:00", "text": "08:00" }, { "value": "08:30", "text": "08:30" },
+{ "value": "09:00", "text": "09:00" }, { "value": "09:30", "text": "09:30" },
+{ "value": "10:00", "text": "10:00" }, { "value": "10:30", "text": "10:30" },
+{ "value": "11:00", "text": "11:00" }, { "value": "11:30", "text": "11:30" },
+{ "value": "12:00", "text": "12:00" }, { "value": "12:30", "text": "12:30" },
+{ "value": "13:00", "text": "13:00" }, { "value": "13:30", "text": "13:30" },
+{ "value": "14:00", "text": "14:00" }, { "value": "14:30", "text": "14:30" },
+{ "value": "15:00", "text": "15:00" }, { "value": "15:30", "text": "15:30" },
+{ "value": "16:00", "text": "16:00" }, { "value": "16:30", "text": "16:30" },
+{ "value": "17:00", "text": "17:00" }, { "value": "17:30", "text": "17:30" },
+{ "value": "18:00", "text": "18:00" }, { "value": "18:30", "text": "18:30" },
+{ "value": "19:00", "text": "19:00" }, { "value": "19:30", "text": "19:30" },
+{ "value": "20:00", "text": "20:00" }, { "value": "20:30", "text": "20:30" },
+{ "value": "21:00", "text": "21:00" }, { "value": "21:30", "text": "21:30" },
+{ "value": "22:00", "text": "22:00" }, { "value": "22:30", "text": "22:30" },
+{ "value": "23:00", "text": "23:00" }, { "value": "23:30", "text": "23:30" }]`
 
 const nuevasHoras = JSON.parse(nuevasHorasJSON);
 
@@ -49,8 +49,10 @@ const nuevasHoras = JSON.parse(nuevasHorasJSON);
 
 for (const nuevaHora of nuevasHoras) {
     let option = document.createElement('option');
-    option.innerText = nuevaHora.text;
-    option.value = nuevaHora.value;
+    //DESESTRUCTURACION //
+    const { text, value } = nuevaHora
+    option.innerText = text;
+    option.value = value;
     horaSelect.appendChild(option);
 }
 
@@ -76,10 +78,10 @@ function calcularHoraDormir(horaDespertar, numeroCiclos) {
     const horaDormir = new Date(horaDormirMS) // Convierto el resultado anterior a un objeto Date
     const horaDormirHora = horaDormir.getHours() //Obtengo las horas del objeto Date
     const horaDormirMinutos = horaDormir.getMinutes() // Obtengo los minutos del objeto Date
-    let horaDormirNueva = horaDormirHora.toString() + ':' + horaDormirMinutos.toString() //Obtengo el resultado concatenando las variables ateriores
+    let horaDormirNueva = `${horaDormirHora.toString()}:${horaDormirMinutos.toString()}` //Obtengo el resultado concatenando las variables ateriores
     //El if agrega un 0 si los minutos obtenidos son 0, para asi mostrar la hora en formato correcto
     if (horaDormirMinutos === 0) {
-        horaDormirNueva = horaDormirNueva + '0'
+        horaDormirNueva = `${horaDormirNueva}0`
     }
 
     return horaDormirNueva
@@ -94,7 +96,8 @@ function calcularHoraDespertar(horaDormir, numeroCiclos) {
     const horaDespertar = new Date(horaDespertarMs)
     const horaDespertarHora = horaDespertar.getHours()
     const horaDespertarMinutos = horaDespertar.getMinutes()
-    let horaDespertarNueva = horaDespertarHora.toString() + ':' + horaDespertarMinutos.toString()
+    let horaDespertarNueva = `${horaDespertarHora.toString()}:${horaDespertarMinutos.toString()}`
+
 
     if (horaDespertarMinutos === 0) {
         horaDespertarNueva = horaDespertarNueva + '0'
@@ -107,14 +110,17 @@ const numeroCiclos = [6, 5, 4]
 function calcular(opcion) {
 
     let horas = []
-    console.log(hora)
+
     switch (opcion) {
         case "1":
             let horaDespertar = hora
             for (const dormir of numeroCiclos) {
                 horas.push(calcularHoraDormir(horaDespertar, dormir))
             }
-            resultado.innerText = 'Para completar ' + numeroCiclos[0] + ' ciclos de sueño, duerme a las ' + horas[0] + '\nPara completar ' + numeroCiclos[1] + ' ciclos de sueño, duerme a las ' + horas[1] + '\nPara completar ' + numeroCiclos[2] + ' ciclos de sueño, duerme a las ' + horas[2];
+
+            resultado.innerText = `Para completar ${numeroCiclos[0]} ciclos de sueño, duerme a las  ${horas[0]}
+            Para completar ${numeroCiclos[1]} ciclos de sueño, duerme a las ${horas[1]}
+            Para completar ${numeroCiclos[2]} ciclos de sueño, duerme a las ${horas[2]}`
             break;
 
         case "2":
@@ -123,7 +129,9 @@ function calcular(opcion) {
                 horas.push(calcularHoraDespertar(horaDormir, despertar))
             }
 
-            resultado.innerText = 'Para completar ' + numeroCiclos[0] + ' ciclos de sueño, despierta a las ' + horas[0] + '\nPara completar ' + numeroCiclos[1] + ' ciclos de sueño, despierta a las ' + horas[1] + '\nPara completar ' + numeroCiclos[2] + ' ciclos de sueño, despierta a las ' + horas[2];
+            resultado.innerText = `Para completar ${numeroCiclos[0]} ciclos de sueño, despierta a las ${horas[0]}
+            Para completar ${numeroCiclos[1]} ciclos de sueño, despierta a las ${horas[1]}
+            Para completar ${numeroCiclos[2]} ciclos de sueño, despierta a las ${horas[2]}`
             break;
 
         default:
